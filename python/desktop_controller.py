8af29abda5e796616709a79e6e0fbc557f6b73bc
P.J. Finlay <pjappdevelopment@gmail.com>
2022-07-31 11:36:47 -0500

    Create DesktopController

--- python/desktop_controller.py
+++ python/desktop_controller.py
@@ -1,7 +1,29 @@
 import ctypes
+import time
 
-desktop_controller = ctypes.CDLL("build/desktop_controller.so")
+InputEventCode = int
 
-desktop_controller.key_press.argtypes = [ctypes.c_int]
+class InputEventCodes:
+    KEY_SPACE = 57
 
-print(desktop_controller.key_press(42))
+class DesktopController:
+    def __init__(self):
+        self.desktop_controller = ctypes.CDLL("build/desktop_controller.so")
+
+        self.desktop_controller.desktop_controller_init.restype = ctypes.c_void_p
+        self.desktop_controller.desktop_controller_free.argtypes = [ctypes.c_void_p]
+        self.desktop_controller.input_event.argtypes = [ctypes.c_void_p, ctypes.c_int]
+
+        self.c_desktop_controller_ptr = self.desktop_controller.desktop_controller_init()
+        time.sleep(1) # Give userspace time to start listening to events
+
+
+    def __del__(self):
+        self.desktop_controller.desktop_controller_free(self.c_desktop_controller_ptr)
+
+    def input_event(self, event : InputEventCode):
+        self.desktop_controller.input_event(self.c_desktop_controller_ptr, event)
+
+desktop_controller = DesktopController()
+
+desktop_controller.input_event(InputEventCodes.KEY_SPACE)
--- src/desktop_controller.c
+++ src/desktop_controller.c
@@ -1,3 +1,73 @@
-int key_press(int key){
-	return key;
+#include <linux/uinput.h>
+
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <stdlib.h>
+
+void emit(int fd, int type, int code, int val) {
+  struct input_event ie;
+
+  ie.type = type;
+  ie.code = code;
+  ie.value = val;
+  /* timestamp values below are ignored */
+  ie.time.tv_sec = 0;
+  ie.time.tv_usec = 0;
+
+  write(fd, &ie, sizeof(ie));
 }
+
+struct desktop_controller {
+  int fd;
+};
+
+void *desktop_controller_init() {
+  struct desktop_controller *desktop_controller_ptr =
+      malloc(sizeof(struct desktop_controller));
+  int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+  desktop_controller_ptr->fd = fd;
+  int eventCode = KEY_SPACE;
+  struct uinput_setup usetup;
+
+  /*
+   * The ioctls below will enable the device that is about to be
+   * created, to pass key events
+   */
+  ioctl(fd, UI_SET_EVBIT, EV_KEY);
+  ioctl(fd, UI_SET_KEYBIT, eventCode);
+
+  memset(&usetup, 0, sizeof(usetup));
+  usetup.id.bustype = BUS_USB;
+  usetup.id.vendor = 0x1234;  /* sample vendor */
+  usetup.id.product = 0x5678; /* sample product */
+  strcpy(usetup.name, "Argos Desktop Controller");
+
+  ioctl(fd, UI_DEV_SETUP, &usetup);
+  ioctl(fd, UI_DEV_CREATE);
+
+  return (void *)desktop_controller_ptr;
+}
+
+void desktop_controller_free(void *self) {
+  struct desktop_controller *desktop_controller_ptr =
+      (struct desktop_controller *)self;
+  ioctl(desktop_controller_ptr->fd, UI_DEV_DESTROY);
+  close(desktop_controller_ptr->fd);
+  free(self);
+}
+
+void input_event(void *self, int eventCode) {
+  struct desktop_controller *desktop_controller_ptr =
+      (struct desktop_controller *)self;
+
+  int fd = desktop_controller_ptr->fd;
+
+  /* Key press, report the event, send key release, and report again */
+  emit(fd, EV_KEY, eventCode, 1);
+  emit(fd, EV_SYN, SYN_REPORT, 0);
+  emit(fd, EV_KEY, eventCode, 0);
+  emit(fd, EV_SYN, SYN_REPORT, 0);
+}
+
